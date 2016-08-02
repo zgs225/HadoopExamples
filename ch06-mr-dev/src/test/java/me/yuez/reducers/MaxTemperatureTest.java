8c6f2390e2b2be95fc063ac302ca93dd62e83282
zgs225 <zgs225@gmail.com>
2016-08-02 23:17:31 +0800

    fix

---
+++ ch06-mr-dev/src/test/java/me/yuez/reducers/MaxTemperatureTest.java
@@ -0,0 +1,21 @@
+package me.yuez.reducers;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MaxTemperatureTest {
+    @Test
+    public void returnsMaximumIntegerValues() throws IOException, InterruptedException {
+        new ReduceDriver<Text, IntWritable, Text, IntWritable>()
+                .withReducer(new MaxTemeratureReducer())
+                .withInput(new Text("1950"),
+                        Arrays.asList(new IntWritable(10), new IntWritable(5)))
+                .withOutput(new Text("1950"), new IntWritable(10))
+                .runTest();
+    }
+}
